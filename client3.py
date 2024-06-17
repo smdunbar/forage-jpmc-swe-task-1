5a2fa5b7253eb677365fd9cc583d689885bb9a72
“sdunbar2022” <simone_dunbar@brown.edu>
2024-06-16 22:40:08 -0400

    feat: complete task, works properly

--- client3.py
+++ client3.py
@@ -42,8 +42,10 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    if price_b == 0:
+    if price_a == 0:
         return 0
+    if price_b == 0:
+        return
     return price_a/price_b
 
 
